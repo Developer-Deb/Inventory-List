e9901624c52bfb93d25c52e312aa52e650b544fc
Debmalya Banerjee <debmalyabanerjee68@gmail.com>
2022-11-22 21:04:39 +0530

    added test cases in the add-item-component

--- src/app/MyComponents/add-items/add-items.component.spec.ts
+++ src/app/MyComponents/add-items/add-items.component.spec.ts
@@ -1,23 +1,50 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgForm } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { AddItemsComponent } from './add-items.component';
 
 describe('AddItemsComponent', () => {
   let component: AddItemsComponent;
   let fixture: ComponentFixture<AddItemsComponent>;
+  let element: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddItemsComponent ]
+      declarations: [ AddItemsComponent,NgForm ],
+      //imports: [NgForm]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(AddItemsComponent);
     component = fixture.componentInstance;
+    element = fixture.debugElement;
     fixture.detectChanges();
+    
   });
 
   it('should create', () => {
+    //console.log(component);
     expect(component).toBeTruthy();
   });
+  it('should match the heading', () =>{
+    let data = fixture.nativeElement;
+    expect(data.querySelector('h4')?.textContent).toEqual('ADD A ITEM TO THE INVENTORY');
+  })
+  it('should match the form title', () =>{
+    let data = fixture.nativeElement;
+    expect(data.querySelector('.form-label')?.textContent).toEqual('Item Name');
+  })
+  it('should be called onSubmit method after submitting the form ', () =>{
+    //creating jasmine spy for creating fake form submit
+    const spy = spyOn(component, 'onSubmit'); 
+    const compiled = fixture.debugElement.nativeElement;
+    const getForm = fixture.debugElement.query(By.css('#form'));
+    //submitting the form
+    getForm.triggerEventHandler('submit', compiled);
+    expect(component.onSubmit).toHaveBeenCalled();
+
+  })
+  
 });
--- src/app/MyComponents/inventory-item/inventory-item.component.spec.ts
+++ src/app/MyComponents/inventory-item/inventory-item.component.spec.ts
@@ -1,23 +1,31 @@
+import { Input, Pipe } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Item } from 'src/app/Item';
 
 import { InventoryItemComponent } from './inventory-item.component';
 
-describe('InventoryItemComponent', () => {
+xdescribe('InventoryItemComponent', () => {
   let component: InventoryItemComponent;
   let fixture: ComponentFixture<InventoryItemComponent>;
+  //let item: Item ;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      //providers:[Item],
       declarations: [ InventoryItemComponent ]
     })
     .compileComponents();
 
+    //item = new Item();
     fixture = TestBed.createComponent(InventoryItemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the app', () => {
+    //console.log(component);
+    //const item = new Item();
+   // component.items = item;
     expect(component).toBeTruthy();
   });
 });
